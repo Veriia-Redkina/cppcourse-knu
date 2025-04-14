2e26dd4fef7bf73ba05b4041a9167cd1fd87a19c
valeriia <valery0082015@gmail.com>
2025-04-14 12:04:26 +0900

    virtual_inheritance ex. -solved

--- exercises/virtual_inheritance/TextBox.cpp
+++ exercises/virtual_inheritance/TextBox.cpp
@@ -10,9 +10,9 @@ void Drawable::draw() const {
 Rectangle::Rectangle(int id, float width, float height) :
     Drawable(id), m_width(width), m_height(height) {}
 
-Text::Text(int id, const std::string &content) :
+Text::Text(int id, std::string content) : // прибираємо const щоб передавалася копія рядка, а не посилання на нього
     Drawable(id), m_content(content) {}
 
-TextBox::TextBox(const std::string &content,
-                 float width, float height) :
-    Rectangle(1, width, height), Text(2, content) {}
+TextBox::TextBox(std::string content,     // прибираємо const
+                 float width, float height) :         
+                 Drawable(3), Rectangle(1, width, height), Text(2, content) {}   //додаємо Drawable(3), що дозволяє задавати правильний ID для кожного елемента при створенні об'єкта TextBox
