93019b47fdb57068afbbe4bb122a60e339297509
Valeriia <valery0082015@gmail.com>
2025-04-14 10:38:48 +0900

    Day 2

--- exercises/virtual_inheritance/TextBox.hpp
+++ exercises/virtual_inheritance/TextBox.hpp
@@ -10,23 +10,23 @@ private:
     int m_id;
 };
 
-class Rectangle : public Drawable {
-public:
+class Rectangle : public virtual Drawable {  //Віртуальне успадкування щоб Drawable буде успадковано лише один раз, незалежно від 
+public:                              //кількості разів інших класів (як Rectangle або Text), які будуть його наслідувати    
     Rectangle(int id, float width, float height);
 protected:
     float m_width;
     float m_height;
 };
 
-class Text : public Drawable {
+class Text : public virtual Drawable {
 public:
-    Text(int id, const std::string &content);
+    Text(int id, std::string content);           //використовувати копію рядка замість посилання
 protected:
     std::string m_content;
 };
 
 class TextBox : public Rectangle, public Text {
 public:
-    TextBox(const std::string &content,
+    TextBox(std::string content,
             float width, float height);
 };
--- exercises/virtual_inheritance/trymultiherit.cpp
+++ exercises/virtual_inheritance/trymultiherit.cpp
@@ -3,12 +3,23 @@
 
 int main() {
     // create a TextBox and call draw
+    TextBox textbox("Hello, World!", 100, 50);
+    //textbox.draw();
 
-
+    /*В результаті маємо:
+   error: ld returned 1 exit status
+    */
     // Fix the code to call both draws by using types
-
-
     // try with virtual inheritance
 
+    Rectangle &r = textbox;
+    r.draw();
+    Text &t = textbox;
+    t.draw();
 
 }
+/*результат
+valeriia@valeriia-HP:~/5k/OOP/cppcourse-knu/exercises/virtual_inheritance$ ./trymultiherit
+Drawing 3
+Drawing 3
+*/
