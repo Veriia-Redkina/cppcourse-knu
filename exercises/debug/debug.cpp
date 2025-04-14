2d4b3e51ddf1af83e0afabf76aee96ea1809507e
valeriia <valery0082015@gmail.com>
2025-04-14 12:14:21 +0900

    Debugging ex. -solved

--- exercises/debug/debug.cpp
+++ exercises/debug/debug.cpp
@@ -32,8 +32,8 @@ int main()
     constexpr auto arraySize = 100;
     int* v = nullptr;
     // create and reverse the vector of LEN numbers
-    reverse(v, 1000);
     v = createAndFillVector(arraySize);
+    reverse(v, arraySize);
 
     // check if the revert worked:
     const bool isReversed = std::is_sorted(v, v + arraySize, std::greater {});
@@ -42,3 +42,30 @@ int main()
 
     return isReversed ? 0 : 1;
 }
+
+/*
+Було виконано компіляцію (g++ -g -o debug debug.cpp) та запуск програми (./debug).
+В результаті в терміналі було записано про помилку Помилка адресування (збережено знімок оперативної пам’яті)
+Для перевірки помилки було запущено gdb ./debug
+
+Reading symbols from ./debug...
+(gdb) run
+Program received signal SIGSEGV, Segmentation fault.
+0x000055555555523d in swap (a=0x0, b=0xf9c) at debug.cpp:6
+6           int c = *a;
+(gdb) backtrace
+#0  0x000055555555523d in swap (a=0x0, b=0xf9c) at debug.cpp:6
+#1  0x00005555555552bb in reverse (v=0x0, len=1000) at debug.cpp:17
+#2  0x0000555555555364 in main () at debug.cpp:35
+
+Виклик функції reverse(v, 1000) призведе до аварійного завершення програми, оскільки звертається до пам'яті через вказівник, який не вказує на жодну область пам'яті
+Виклик функції reverse до того, як ініціалізується v, є помилкою, бо немає значення в векторі, щоб його перевертати
+
+Після виправленьмаємо (./debug):
+Reading symbols from ./debug...
+(gdb) run
+Vector reversed successfully: true
+[Inferior 1 (process 167255) exited normally]
+(gdb) backtrace
+No stack.
+*/
